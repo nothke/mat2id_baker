3155dc75daf9c289b46b63048fbfb182b997b0e4
Ivan Notaros <nothke@gmail.com>
2021-12-08 21:11:43 +0100

    Now it's an operator

--- bake_mat_to_texture.py
+++ bake_mat_to_texture.py
@@ -5,13 +5,14 @@ from PIL import Image, ImageDraw, ImageFilter
 import time
 
 # ---- PROPERTIES ----
-
 tex_size = 256
 file_path = "baked_test.png"
 supersample = 2
 
 
 # ---- CODE ----
+
+# static functions:
 
 def get_material_color(mat):
     nodes = mat.node_tree.nodes
@@ -26,6 +27,7 @@ def get_material_color(mat):
 
     return color
 
+
 def get_blend_folder():
     if not bpy.data.is_saved:
         raise Exception("Cannot bake as the blend file is not yet saved")
@@ -38,58 +40,101 @@ def get_blend_folder():
 
 # ---- MAIN ----
 
-folder_path = get_blend_folder()
 
-start_time = time.time()
+def main(context):
+    print("Running")
 
-# prepare
-me = bpy.context.object.data
-uv_layer = me.uv_layers.active.data
+    folder_path = get_blend_folder()
 
-mats = me.materials
+    start_time = time.time()
 
-if len(mats) is 0:
-    raise Exception("Object has no materials")
+    # prepare
+    me = bpy.context.object.data
+    uv_layer = me.uv_layers.active.data
 
-# make image
-size = tex_size
-for i in range(supersample):
-    size *= 2
+    mats = me.materials
 
-img = Image.new('RGB', (size, size), color='black')
-draw = ImageDraw.Draw(img)
+    if len(mats) == 0:
+        raise Exception("Object has no materials")
 
-# cache colors
-mat_colors = []
+    # make image
+    size = tex_size
+    for i in range(supersample):
+        size *= 2
 
-for mat in mats:
-    col = get_material_color(mat)
-    mat_colors.append(
-        (int(col[0] * 255), int(col[1] * 255), int(col[2] * 255)))
+    img = Image.new('RGB', (size, size), color='black')
+    draw = ImageDraw.Draw(img)
 
-# go through polygons and render them on the texture
-for poly in me.polygons:
-    #print("Polygon index: %d, length: %d" % (poly.index, poly.loop_total))
-    #print("Loop index: %d" % (poly.material_index))
-    col = mat_colors[poly.material_index]
-    #print("Color: " + str(mat_colors[poly.material_index]))
+    selected_objects = []
+    for obj in context.selected_objects:
+        if obj.type != "MESH":  # Skip non-mesh
+            obj.select_set(False)
+            continue
 
-    uvs = []
-    for loop_index in range(poly.loop_start, poly.loop_start + poly.loop_total):
-        #print("    Vertex: %d" % me.loops[loop_index].vertex_index)
-        #print("    UV: %r" % uv_layer[loop_index].uv)
-        uv = uv_layer[loop_index].uv
-        uvs.append((uv.x * size, uv.y * size))
+    # cache colors
+    mat_colors = []
 
-    draw.polygon(uvs, fill=col)
+    for mat in mats:
+        col = get_material_color(mat)
+        mat_colors.append(
+            (int(col[0] * 255), int(col[1] * 255), int(col[2] * 255)))
 
-#img_blurred = img.filter(ImageFilter.GaussianBlur(radius=6))
-#img = img_blurred
-#img = Image.blend(img, img_blurred, 0.5)
+    # go through polygons and render them on the texture
+    for poly in me.polygons:
+        #print("Polygon index: %d, length: %d" % (poly.index, poly.loop_total))
+        #print("Loop index: %d" % (poly.material_index))
+        col = mat_colors[poly.material_index]
+        #print("Color: " + str(mat_colors[poly.material_index]))
 
-img = img.resize((tex_size, tex_size), resample=Image.BICUBIC)
+        uvs = []
+        for loop_index in range(poly.loop_start, poly.loop_start + poly.loop_total):
+            #print("    Vertex: %d" % me.loops[loop_index].vertex_index)
+            #print("    UV: %r" % uv_layer[loop_index].uv)
+            uv = uv_layer[loop_index].uv
+            uvs.append((uv.x * size, uv.y * size))
 
-#img.show()
-img.save(folder_path + file_path, "PNG")
+        draw.polygon(uvs, fill=col)
 
-print("--- Finished baking in %s seconds ---" % (time.time() - start_time))
+    #img_blurred = img.filter(ImageFilter.GaussianBlur(radius=6))
+    #img = img_blurred
+    #img = Image.blend(img, img_blurred, 0.5)
+
+    img = img.resize((tex_size, tex_size), resample=Image.BICUBIC)
+
+    # img.show()
+    img.save(folder_path + file_path, "PNG")
+
+    print("--- Finished baking in %s seconds ---" % (time.time() - start_time))
+
+
+# ---- BLENDER ----
+
+class NOTHKE_OT_mat2id_baker(bpy.types.Operator):
+    """Bakes material colors to id texture without Cycles"""
+    bl_idname = "object.mat2id_baker"
+    bl_label = "mat2id baker"
+
+    @classmethod
+    def poll(cls, context):
+        return context.active_object is not None
+
+    def execute(self, context):
+        main(context)
+        return {'FINISHED'}
+
+# ---- REGISTRATION ----
+
+
+def register():
+    bpy.utils.register_class(NOTHKE_OT_mat2id_baker)
+
+
+def unregister():
+    bpy.utils.unregister_class(NOTHKE_OT_mat2id_baker)
+
+
+if __name__ == "__main__":
+    register()
+
+# test call
+bpy.ops.object.mat2id_baker()
